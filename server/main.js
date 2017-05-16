e6d53a61b04031a16de21b10c176bc8169eaed24
Rodrigo Sanabria <r.sanabriadc@gmail.com>
2017-05-15 21:02:46 -0500

    socket chart y bootstrap

--- server/main.js
+++ server/main.js
@@ -17,30 +17,46 @@ app.use(express.static('public'));
 });*/
 
 io.on('connection', function(socket) {
-  console.log('Alguien se ha conectado con Sockets');
-  Cylon.robot({
-    connections: {
-      arduino: { adaptor: 'firmata', port: '/dev/ttyUSB0' }
-    },
-    devices: {
-     button: { driver: 'analog-sensor', pin: 2, lowerLimit: 0, upperLimit: 1023}
-    },
-    work: function(my) {
-      every((0.5).second(), function(){
-        var push = 0;
-        push = my.button.analogRead();
-        console.log('push: ', push);
-        socket.emit('push', push);
-      });
-    }
-  }).start()
-    /*socket.on('new-message', function(data) {
-      messages.push(data);
+    console.log('Alguien se ha conectado con Sockets');
+    /*  Cylon.robot({
+        connections: {
+          arduino: { adaptor: 'firmata', port: '/dev/ttyUSB0' }
+        },
+        devices: {
+         button: { driver: 'analog-sensor', pin: 2, lowerLimit: 0, upperLimit: 1023}
+        },
+        work: function(my) {
+          every((0.5).second(), function(){
+            var push = 0;
+            push = my.button.analogRead();
+            console.log('push: ', push);
+            socket.emit('push', push);
+          });
+        }
+      }).start()*/
+    socket.on("iniciar", () => {
+        console.log("Inicio");
+        var contador = 0;
+        setInterval(() => {
+            data = { label: contador, data: Math.floor(Math.random() * 5) + 1 }
+            io.sockets.emit('data', data);
+            contador += 1;
+        }, 500);
 
-      io.sockets.emit('push',push);
-    });*/
+    });
+
+    socket.on('disconnect', () => {
+            console.log("desconexión");
+            activo = false;
+            contador = 0;
+        })
+        /*socket.on('new-message', function(data) {
+          messages.push(data);
+
+          io.sockets.emit('push',push);
+        });*/
 });
 
 server.listen(8080, function() {
-  console.log('Servidor corriendo en http://localhost:8080');
+    console.log('Servidor corriendo en http://localhost:8080');
 });
